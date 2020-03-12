f5cb745c706a1e0ed4e2f1096b21a74aab27c95b
William Cofer <willcofer@gmail.com>
2020-03-12 14:16:23 -0500

    database handler

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
-const db = require('./sql');
+const sqlite3 = require('sqlite3');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = requre('body-parser');
+const db = new sqlite3.Database('./nashville_t.sqlite ');
 PORT = 4002;
 
 
 
-//select * from 'test_table'
-//where end_date < CAST('2009-12-15' AS DATE)
-//or start_date > CAST('2010-01-02' AS DATE)
+/*select * from 'test_table'
+where end_date < CAST('2009-12-15' AS DATE)
+or start_date > CAST('2010-01-02' AS DATE) */
+
 app.get('/bookings',(req,res,next) => {
-start = req.query.start_date;
-end = req.query.end_date;
-db.get('SELECT Property_name FROM Bookings WHERE ($start NOT >= Start_Date AND $end NOT End_Date) OR  ',{$start: start, $end: end}, (err,row) => {
+start = req.query.startDate;
+end = req.query.endDate;
+db.get('SELECT DISTINCT Property_name FROM Bookings WHERE ($end < Start_Date OR $start >= End_Date)',{$start: start, $end: end}, (err,row) => {
     if (err) {
         console.log("error while searching for available properties");
         console.log(err);
     }
     if (row) {
-        res.send({property: rows});
-    }   
+        res.send({bookings: rows});
+    }  else {
+        console.log('No rows were recorded');
+    } 
 });
 next();
 });
 
-app.post('/bookings',(req,res,next) => {
+/*app.post('/bookings',(req,res,next) => {
 start = req.query.start_date;
 end = req.query.end_date;
 //Name of Home
 property = req.query.property;
 db.run('INSERT INTO Bookings (Property_Name, Start_Date, End_Date) VALUES ($property, $start, $end)',{$property:property, $start: start, $end: end});
-});
+}); */
 
 
 
--- sql.js
+++ sql.js
@@ -4,12 +4,34 @@ var db =  new sqlite3.Database('./nashville_t.sqlite');
 
 db.serialize(() => {
     db.run('DROP TABLE IF EXISTS Bookings');
-    db.run('CREATE TABLE Bookings (id INTEGER PRIMARY KEY, Property_Name TEXT NOT NULL, Start_Date INTEGER NOT NULL, End_Date NOT NULL)', (err) => {
-        console.log(err);
+    db.run('CREATE TABLE Bookings (id INTEGER PRIMARY KEY, Property_Name TEXT NOT NULL, Start_Date INTEGER NOT NULL, End_Date INTEGER NOT NULL)', (err,rows) => {
+        if (err) {
+            console.log(err);
+            console.log("There was an error during the creation of Bookings");
+        }
+        
     });
-    db.run('SELECT * FROM Bookings');
-})
-
+    db.run("INSERT INTO Bookings (Property_Name, Start_Date, End_Date) VALUES ( 'Property_1', '20200404', '20200407')", (err,rows) => {
+        if (err) {
+            console.log(err)
+            console.log("Error during insertion into bookings");
+        }
+        if (rows) {
+            console.log(rows);
+        }
+        
+    })
+    
+});
+db.run('SELECT * FROM Bookings', (err,rows) => {
+    if (err) {
+        console.log(err);
+    } 
+    if (rows) {
+        console.log(rows)
+    }
+    
+});
 
 
 
