6ec51afb8127e0c4e7dfb958ce0a3324536effd0
William Cofer <willcofer@gmail.com>
2020-04-09 16:05:03 -0500

    redux form validation

--- src/Components/Modal/StrapModal.js
+++ src/Components/Modal/StrapModal.js
@@ -1,147 +1,220 @@
 import React from 'react' ;
-import { Modal, Container, Row, Col, Button, InputGroup, FormControl, Form } from 'react-bootstrap';
+import { Modal, Container, InputGroup, FormControl, Form } from 'react-bootstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Row, Button, Label, Col } from 'reactstrap';
 
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const isNumber = val => !isNaN(+val);
+const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
-export function StrapModal(props,propsListing,handleChange,post) {
-    
-        return (
-            <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
-              <Modal.Header closeButton>
-              <Modal.Title id="contained-modal-title-vcenter">
-                {props.homeAddress} <br/> 
-                </Modal.Title>
-                <div className="mt-2 ml-3 text-secondary">
-                {props.start} - {props.end}
-                </div>
-               
-              </Modal.Header>
-              <Modal.Body>
-                <Container>
-                  <Row className="show-grid">
-                    <Col xs={12} md={12}>
-                    </Col>
-                    <Col xs={12} md={12}>
-                      
-                    </Col>
-                  </Row>
-                  <Row className="show-grid">
-                  <Form>
-  <Form.Row>
-  <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                {props.email}
-              </Form.Group>
-    
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                {props.password}
-              </Form.Group>
-  </Form.Row>
 
-  <Form.Group controlId="formGridAddress1">
-    <Form.Label>Address</Form.Label>
-    {props.address}
-  </Form.Group>
+export class StrapModal extends React.Component {
+    constructor(props) {
+      super(props)
 
-  <Form.Group controlId="formGridAddress2">
-    <Form.Label>Address 2</Form.Label>
-    <Form.Control placeholder="Apartment, studio, or floor" />
-  </Form.Group>
+      this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            address: '',
+            city: '',
+            zipCode: '',
+            touched: {
+                firstName: false,
+                lastName: false,
+                email: false,
+                password: false,
+                address: false,
+                city: false,
+                zipCode: false
 
-  <Form.Row>
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>City</Form.Label>
-      <Form.Control />
-    </Form.Group>
+            }
+      };
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    <Form.Group as={Col} controlId="formGridState">
-      <Form.Label>State</Form.Label>
-      <Form.Control as="select" value='option'> 
-			<option value="AK">Alaska</option>
-			<option value="AL">Alabama</option>
-			<option value="AR">Arkansas</option>
-			<option value="AZ">Arizona</option>
-			<option value="CA">California</option>
-			<option value="CO">Colorado</option>
-			<option value="CT">Connecticut</option>
-			<option value="DC">District of Columbia</option>
-			<option value="DE">Delaware</option>
-			<option value="FL">Florida</option>
-			<option value="GA">Georgia</option>
-			<option value="HI">Hawaii</option>
-			<option value="IA">Iowa</option>
-			<option value="ID">Idaho</option>
-			<option value="IL">Illinois</option>
-			<option value="IN">Indiana</option>
-			<option value="KS">Kansas</option>
-			<option value="KY">Kentucky</option>
-			<option value="LA">Louisiana</option>
-			<option value="MA">Massachusetts</option>
-			<option value="MD">Maryland</option>
-			<option value="ME">Maine</option>
-			<option value="MI">Michigan</option>
-			<option value="MN">Minnesota</option>
-			<option value="MO">Missouri</option>
-			<option value="MS">Mississippi</option>
-			<option value="MT">Montana</option>
-			<option value="NC">North Carolina</option>
-			<option value="ND">North Dakota</option>
-			<option value="NE">Nebraska</option>
-			<option value="NH">New Hampshire</option>
-			<option value="NJ">New Jersey</option>
-			<option value="NM">New Mexico</option>
-			<option value="NV">Nevada</option>
-			<option value="NY">New York</option>
-			<option value="OH">Ohio</option>
-			<option value="OK">Oklahoma</option>
-			<option value="OR">Oregon</option>
-			<option value="PA">Pennsylvania</option>
-			<option value="PR">Puerto Rico</option>
-			<option value="RI">Rhode Island</option>
-			<option value="SC">South Carolina</option>
-			<option value="SD">South Dakota</option>
-			<option value="TN">Tennessee</option>
-			<option value="TX">Texas</option>
-			<option value="UT">Utah</option>
-			<option value="VA">Virginia</option>
-			<option value="VT">Vermont</option>
-			<option value="WA">Washington</option>
-			<option value="WI">Wisconsin</option>
-			<option value="WV">West Virginia</option>
-			<option value="WY">Wyoming</option>
-      </Form.Control>
-    </Form.Group>
+    handleSubmit(values) {
+      console.log('current state is: ' + JSON.stringify(values));
+      alert('Current state is: ' + JSON.stringify(values));
+      
+  }
 
-    <Form.Group as={Col} controlId="formGridZip">
-      <Form.Label>Zip</Form.Label>
-      <Form.Control />
-    </Form.Group>
-  </Form.Row>
-  <Form.Row>
-  <Col xs={12} md={12} className="mb-3">
-    ${props.perNight} x {props.numNights} Nights
+
+    render(){
+      let {numNights, perNight, cleaningFee, start, end} = this.props;
+      let subTotal = numNights * perNight;
+      let tax = (numNights * perNight)* 0.10;
+      
+      return (
+        <Modal {...this.props} aria-labelledby="contained-modal-title-vcenter">
+          <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {this.props.homeAddress} <br/> 
+            </Modal.Title>
+            <div className="mt-2 ml-3 text-secondary">
+            {this.props.start} - {this.props.end}
+            </div>
+           
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Row className="show-grid">
+                <Col xs={12} md={12}>
+                </Col>
+                <Col xs={12} md={12}>
+
+                </Col>
+              </Row>
+              <Row className="show-grid">
+              <LocalForm onSubmit={values => this.handleSubmit(values)}>
+
+            <Row className="form-group" row>
+            <Label htmlFor="email" md={2}>Email</Label>
+            <Col className="mb-2" md={10}>            
+            <Control.text
+            id="email"
+            name="email" 
+            model=".email"
+            className="form-control"
+            placeholder="Enter email"
+            validators = {{
+
+            }}
+            />
+            </Col>
+            </Row>
+            <Row className="form-group" row>
+            <Label htmlFor="password" md={2}>Password</Label>
+            <Col md={10}>
+            <Control.text id="passoword" 
+            name="password" 
+            model=".password"
+            className="form-control"
+            placeholder="Password" 
+            validators = {{
+
+            }}
+            />
+            </Col>
+            </Row>
+          
+
+          <Row className="form-group">
+          <Label htmlFor="address" md={2} >Address</Label>
+          <Col md={10}>
+          <Control.text id="address" 
+          name="address" 
+          model=".address"
+          className="form-control"
+          placeholder="Address" 
+          validators= {{
+
+          }}
+          />
+          <Errors
+          className="text-danger"
+          model=".lastName"
+          show="touched"
+          component="div"
+          messages={{
+          required: 'Required',
+          minLength: 'Must be at least 2 characters',
+          maxLength: 'Must be 15 characters or less'
+          }}
+                                              />
+          </Col>
+          </Row>
+
+
+    <Row className="form-group" row>
+    <Label htmlFor="city" md={2} >City</Label>
+      <Col md={10}>
+      <Control.text  id="city" name="city"
+        model=".city"  
+        className="form-control"  
+        placeholder="City" 
+        validators={{ 
+        required, 
+        minLength: minLength(4),
+        maxLength: maxLength(15)
+        }}                                        
+        /> 
+      </Col>
+      </Row>
+
+<Row className="form-group" row>
+<Label htmlFor="state" md={2}>State</Label>
+<Col md={10}>
+  <Control.select 
+    model=".state"
+   className="form-control"
+   id="state" name="state"
+   placeholder="State"
+   validators={{
+   required
+  }}                   
+   />
   </Col>
-  <Col xs={12} md={12} className="mb-3">
-     +  ${props.cleaningFee} Cleaning Fee
-  </Col>
-  <Col xs={12} md={12} className="mb-3">
-    +  tax
-  </Col>
-  </Form.Row>
-  <hr></hr>
+
+</Row>
+
+<Row className="form-group" row>
+<Label htmlFor="zipCode" md={2}>Zip</Label>
+  <Col md={10}>
+  <Control.text
+  model=".zipCode"
+  className="form-control"
+  id="zipCode" name="zipCode"
+  placeholder="Zip Code"
+  validators={{
+      required, 
+      minLength: minLength(5),
+      maxLength: maxLength(5)
+  }} 
   
-  <Button className="btn-primary mr-3" type="submit" oncClick={props.reserve}> Reserve</Button>
-    <Button className="btn-secondary mr-3" onClick={props.onHide}>Close</Button>
-    <a href="/">or Login</a> 
-</Form>
-                  </Row>
-                </Container>
-              </Modal.Body>
-            </Modal>
-          )
+  />
+  </Col>
+  </Row>
+
+<Row className="form-group">
+<Col xs={12} md={12} className="mb-3">
+${this.props.perNight} x {this.props.numNights} Nights
+</Col>
+<Col xs={12} md={12} className="mb-3">
+ +  ${this.props.cleaningFee} Cleaning Fee
+</Col>
+<Col xs={12} md={12} className="mb-3">
++  ${tax} (10%)
+</Col>
+<Col xs={12} md={12} className="mb-3">
+Total: Total
+</Col>
+</Row>
+<hr></hr>
+
+<Button className="btn-primary mr-3" type="submit" oncClick={this.props.reserve}> Reserve</Button>
+<Button className="btn-secondary mr-3" onClick={this.props.onHide}>Close</Button>
+<a href="/">or Login</a> 
+</LocalForm>
+              </Row>
+            </Container>
+          </Modal.Body>
+        </Modal>
+      )
+
+    }
+       
     
     
   }
   
   export default StrapModal;
+
+
+  
+            
+            
