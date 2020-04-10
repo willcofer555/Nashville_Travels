a9a46957ed67ce4235f5ed330625da03d9387ff9
William Cofer <willcofer@gmail.com>
2020-04-09 21:05:21 -0500

    book user table

--- sql.js
+++ sql.js
@@ -7,17 +7,35 @@ const db = new sqlite3.Database(':memory', (err => {
 
 db.serialize(() => {
     db.run('DROP TABLE IF EXISTS Bookings');
-    db.run('CREATE TABLE Bookings (id INTEGER PRIMARY KEY, Property_Name TEXT NOT NULL, Start_Date INTEGER NOT NULL, End_Date INTEGER NOT NULL)', (err,rows) => {
+    db.run('DROP TABLE IF EXISTS Users');
+    db.run('CREATE TABLE Bookings (id INTEGER PRIMARY KEY, Property_Name TEXT NOT NULL, Start_Date INTEGER NOT NULL, End_Date INTEGER NOT NULL, Email TEXT NOT NULL)', (err,rows) => {
         if (err) {
             console.log(err);
-            console.log("There was an error during the creation of Bookings");
+            console.log("There was an error during the creation of Bookings table");
         }
         
     });
-    db.run("INSERT INTO Bookings (Property_Name, Start_Date, End_Date) VALUES ( 'Property_1', '20200404', '20200407')", (err,rows) => {
+    db.run('CREATE TABLE Users (id INTEGER PRIMARY KEY, Property_Name TEXT NOT NULL, Last_Name TEXT NOT NULL, First_Name TEXT NOT NULL, Email TEXT NOT NULL, City TEXT NOT NULL, State TEXT NOT NULL, Zipcode INTEGER NOT NULL)', (err,rows) => {
+        if (err) {
+            console.log(err);
+            console.log("There was an error during the creation of Users table")
+        }
+    });
+
+    db.run("INSERT INTO Users (Property_Name, Last_Name, First_Name, Email, City, State, Zipcode) VALUES ('Property_1', 'Doe', 'John', 'Johndoe@gmail.com', 'Franklin', 'Tennessee', '37064')", (err,rows) => {
         if (err) {
             console.log(err)
-            console.log("Error during insertion into bookings");
+            console.log("Error during insertion into users table");
+        } 
+        if (rows) {
+            console.log(rows);
+        }
+    });
+
+    db.run("INSERT INTO Bookings (Property_Name, Start_Date, End_Date, Email) VALUES ( 'Property_1', '20200404', '20200407', 'Johndoe@gmail.com')", (err,rows) => {
+        if (err) {
+            console.log(err)
+            console.log("Error during insertion into bookings table");
         }
         if (rows) {
             console.log(rows);
@@ -25,6 +43,7 @@ db.serialize(() => {
     })
     
 });
+
 db.run('SELECT * FROM Bookings', (err,rows) => {
     if (err) {
         console.log(err);
@@ -34,6 +53,14 @@ db.run('SELECT * FROM Bookings', (err,rows) => {
     }
     
 });
+db.run('SELECT * FROM Users', (err,rows) => {
+    if (err) {
+        console.log(err);
+    }
+    if (rows) {
+        console.log(rows)
+    }
+})
 
 
 
