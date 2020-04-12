caf92d152bf0f5c8086710bf233e061eb824ef7f
William Cofer <willcofer@gmail.com>
2020-04-11 22:29:48 -0500

    change import directories

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header/Header';
-import Lower from './Components/Header/Lower';
+import Main from './Components/Main';
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
+    <Main/>
     </BrowserRouter>
   );
 }
