cab4d7867c69f1eb95646948368a479c075b42db
William Cofer <willcofer@gmail.com>
2020-05-20 22:14:08 -0500

    migration to folder

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -7,25 +7,44 @@ class Login extends React.Component {
     constructor(props) {
         super(props);
 
-        this.props.logout();
+        
 
         this.state = {
             username: '',
             password: '',
             submitted: false
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        if (username && password) {
+            this.props.login(username, password);
+        }
+    }
     
 
 
     render() {
+        const { loggingIn } = this.props;
+        const { username, password, submitted } = this.state;
         return(
             <div className="container-fluid">
                 <Nav />
                 <form className="formWidth">
                     <div className="loginDiv">
-                    <h3>Nashville Travels</h3>
+                    <h3>Welcome Home. </h3>
                     </div>
                     <div className="form-group">
                         <label>Email address</label>
@@ -49,7 +68,6 @@ class Login extends React.Component {
                         Forgot <a href="#">password?</a>
                     </p>
                 </form>
-    
             </div>
         )
 
